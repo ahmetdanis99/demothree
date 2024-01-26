8951b4b08e8c73727bfa30270d19713f140fcd55
Ahmet Danış <ahmetdanis99@gmail.com>
2024-01-26 04:22:40 +0300

    fix

--- src/qrcode/Armchairqr.jsx
+++ src/qrcode/Armchairqr.jsx
@@ -1,3 +1,9 @@
 export default function Armchairqr() {
-  return <img className="aspect-square" src="/src/assets/qrcode/armchair.png" alt="Koltuk QR Kod" />;
+  return (
+    <img
+      className="aspect-square"
+      src="../assets/qrcode/armchair.png"
+      alt="Koltuk QR Kod"
+    />
+  );
 }
